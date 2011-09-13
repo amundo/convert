9d11dd1a079e7debd2f258f441ee8eb2a73fe295
Patrick Hall <pathall@gmail.com>
2011-09-12 21:16:57 -0700

    backbone version

--- js/notebook.js
+++ js/notebook.js
@@ -1,38 +1,46 @@
 $(function(){
 
-  var data = null;
-  $.getJSON('js/subfile.json', function(resp){
-    storeRules(resp);
-  })
 
-  function storeRules(data){
-    $('#rules').data('rules', data);
-    $.each(data, function(i, datum){
-      $('#rules').append('<strong>'+datum[0]+'</strong>  <span>'+datum[1]+'</span>');
-    })
+  window.Sentence = Backbone.Model.extend({
+    initialize: function(){
+      this.timestamp = new Date().getTime();
+    }    
+  });
+
+  window.Sentences = Backbone.Collection.extend({
+    url : 'js/mandarin.json',
+    model : Sentence,
+  });
+
+  window.NotebookView = Backbone.View.extend({
+    el : $('#notebook') ,
+
+    initialize : function(){
+      _.bindAll(this, 'render');
+    },
+
+    render : function(){
+    }
+  });
+
+  function Notebook(){
+
+    window.sentences = new Sentences({
+      url: 'js/notebook.js'
+    });
+
+    window.notebookView = new NotebookView({
+      collection: sentences
+    });
+
+    this.start = function(){
+      sentences.reset();
+      notebookView.render();
+      //Backbone.history.start();
+    };
+
   }
 
-  function convert(text){
-    var rules = $('#rules').data('rules');
-
-    rules.sort(function(a,b){ return b[0].length < a[0].length ? -1 : 1; });
-
-    $.each(rules, function(i, rule){
-      var beforeRE = new RegExp(rule[0], 'g'),
-          after = rule[1];
-
-      text = text.replace(beforeRE, after);
-
-    })
-    return text;
-  }
-
-  $('#plain').keyup(function(){
-
-    var before = $('#plain').attr('value');
-    var after = convert(before);
-    $('#transliterated').attr('value', after);
-
-  })
+  window.notebook = new Notebook().start();
 
 })
