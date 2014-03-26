9bed9bc10891367a15b90a9111c038792032918f
Patrick Hall <pat@amundo.com>
2014-03-26 12:47:48 -0700

    added a commandline tool for inserting a new alias

---
+++ ualias.py
@@ -0,0 +1,21 @@
+import json
+import sys
+
+SUBFILE =  '/Users/pat/Sites/Writing/convert/js/subfile.json'
+data = json.load(open(SUBFILE))
+
+if len(sys.argv) != 3: 
+  print 'Usage: python ualias.py  <alias> <codepoint>'
+  exit()
+
+alias, code = sys.argv[1], sys.argv[2]
+
+uchar = unichr(int(code, 16))
+
+data.append( [ 
+  '{' + alias + '}',
+  uchar
+]) 
+  
+
+open(SUBFILE, 'w').write(json.dumps(data, indent=2))
